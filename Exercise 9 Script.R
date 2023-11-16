c335e4b7034dac54a115e857fce06e81035884d0
John LeSage <jlesage@nd.edu>
2023-11-16 15:33:58 -0500

    Setting up Q2

--- Exercise 9 Script.R
+++ Exercise 9 Script.R
@@ -8,4 +8,8 @@ library(ggplot2)
 library(cowplot)
 temps <- read.table("temps.txt", header=TRUE, sep=",", stringsAsFactors=FALSE)
 #setting up data frame and appropriate R packages
-ggplot(data=temps, aes(x=Latitude, y=AvgHigh)) + geom_point() + xlab("Latitude (degrees N") + ylab("Avg. March High (degrees F)") + stat_smooth(method = "lm")
+ggplot(data=temps, aes(x=Latitude, y=AvgHigh)) + geom_point() + xlab("Latitude (degrees N)") + ylab("Avg. March High (degrees F)") + stat_smooth(method = "lm")
+
+#Question 2
+data <- read.table("data.txt", header=TRUE, sep=",", stringsAsFactors=FALSE)
+#loads the "data.txt" file into R, where it is stored as a data frame
